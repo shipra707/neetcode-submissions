4361a98bb73387601db8fbfc7c6bed078639c5fe
shipra707 <shipra.24b01330003@abes.ac.in>
2026-07-11 22:39:16 +0530

    Add: rotate-array - submission-0

---
+++ Data Structures & Algorithms/rotate-array/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+           ans[(i+k)%n]=nums[i]; 
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=ans[i];
+        }
+    }
+};
