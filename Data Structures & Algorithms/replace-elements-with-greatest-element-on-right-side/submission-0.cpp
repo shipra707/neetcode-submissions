8d771e031d44c9347b37dda0d1f85fbf9e1c17a3
shipra707 <shipra.24b01330003@abes.ac.in>
2026-07-16 17:09:10 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-0

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int maxi=-1;
+        for(int i=arr.size()-1;i>=0;i--){
+            int cur=arr[i];
+            arr[i]=maxi;
+            maxi=max(maxi,cur);
+        }
+        return arr;
+    }
+};
