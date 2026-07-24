22d402becc7bfe68ee086fd7433b40124cb2d395
shipra707 <shipra.24b01330003@abes.ac.in>
2026-07-24 21:49:09 +0530

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]==' '&& count==0){
+                continue;
+            }
+            if(s[i]==' '){
+                break;
+            }
+            count++;
+        }
+        return count;
+    }
+};
